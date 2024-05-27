3ccdeeeca071d5c183f70f667c2bd574df7a3479
hide338 <work.8a5x2a@gmail.com>
2024-05-28 02:30:07 +0900

    fv correction

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 </body>
 </html>
